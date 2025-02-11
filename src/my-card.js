0959369806c68b194eab3b42038ad84f2099fb27
Ryan-Mindick <rjm7113@psu.edu>
2025-02-11 13:16:16 -0500

    new changes
    new changes to card and index 2/11

--- src/my-card.js
+++ src/my-card.js
@@ -15,30 +15,76 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
-    this.image = "https://via.placeholder.com/150";
+    this.image = "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3929630.png&w=350&h=254";
+    this.link = "https://hax.psu.edu/";
+    this.description = "";
+    this.fancy = false;
   }
 
   static get styles() {
     return css`
       :host {
         display: block;
+      
+      //:host ([fancy]) .card {
+      //  background-color: blue;
+      //  color: white;
+      
+        .button {
+          padding: 6px;
+          border-radius: 20px;
+          width: 100px;
+          font-family: Comic Sans MS;
+        }
+
+        .card {
+          background-color: #45a717;
+          padding: 4px;
+          width: 350px;
+          height: 475px;
+          border-radius: 20px;
+          margin: 10px
+        }
+
+        .h1 {
+          font-size: 20px;
+          font-family: Comic Sans MS;
+        }
+
+        .h2 {
+          font-size: 15px;
+          font-family: Comic Sans MS;
+        }
       }
     `;
   }
 
   render() {
     return html`
-    <div>
-      <img src="${this.image}" alt="${this.title}"/>
-    </div>`;
+      <div id="cardlist">
+        <div class="card">
+          <h1>${this.title}</h1> 
+          <h1>Philadelphia Eagles</h1>
+            <img src="${this.image}" alt="${this.title}"/>
+          <h2>${this.description}</h2>
+            <button>
+              <a class="button" href="https://hax.psu.edu/">Details</a>   
+            </button>
+        </div>
+      </div>`;
   }
 
   static get properties() {
     return {
+      fancy: { type: Boolean, reflect: true },
       title: { type: String },
       image: { type: String },
+      link: { type: String },
+      description: { type: String},
     };
   }
 }
 
 globalThis.customElements.define(MyCard.tag, MyCard);
+
+
